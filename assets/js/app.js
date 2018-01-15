6df1f04d9c7dadd2f75dc399cb2c79ecf6e0606d
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-01-14 23:48:01 -0600

    completed game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,68 +1,184 @@
 $(document).ready(function(){
-//Countdown Function
-var gameTime = 10;
-    
-//Right and Wrong Answer Log
-    var rightAnswers = 0;
-    var wrongAnswers = 0;
-    var unAnswered = 0;
+  //Right and Wrong Answer Log
+  var rightAnswers = 0;
+  var wrongAnswers = 0;
+  var unAnswered = 6;
 
-
-//Show and hide for game divs
-
-    //console.log("Document Load is working");
-    function hideGame(){
-        $("#startScreen").show();
-        //$("#gameView").hide();
-        //$("#results").hide();
-        console.log("hide game is working");
+    //Uncheck all radios
+    function clearRadios(){
+    $("#gameView input").prop('checked', false);
     }
-    hideGame();
-    
-    function radioBuilder() {
-          var usernames = [
-            'test1', 'test2', 'test3', 'test4'
-          ];
+    //Countdown Function
+    var gameTime = 60;
+    var intervalId;
+    function decrement() {
+      $("#ticker").text(gameTime--);
+      if (gameTime === 11){
+        $("#ticker").css("color", "red");
+      }
+      if (gameTime === -1){
+        console.log("game ticker detector is working");
+        stop();
+        $("#startScreen").hide();
+        $("#gameView").hide();
+        $("#results").show();
+      }
+  	}
+    function stop() {
+    	clearInterval(intervalId);
+    }
+    function gameTicker(){
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
 
-          var result = $("#radioBuild");
-            var tabIndexSet = 1;
-          result.html('');
+    };
 
-          for (var i = 0; i < usernames.length; i++) {
-            result.append('<label><input type="radio" name="usernames" value="' + usernames[i] + '"tabindex="' + tabIndexSet++ + '" />' + usernames[i] + '</label>');
-        
-//        for (var i = 0; i < usernames.length; i++) {
-//            result.append('<form><fieldset><h3>Question' + questionHeading++ + '<label><input type="radio" name="usernames" value="' + usernames[i] + '"tabindex="' + tabIndexSet++ + '" />' + usernames[i] + '</label></fieldset></form>');
-//              
-          } 
-        }
-    
+    //Game Start
     function gameStart(){
         $("#startScreen a").on("click", function(){
+          $("#startScreen").hide();
+          $("#gameView").show();
+          $("#results").hide();
+          console.log("Button Click is working");
+          gameTicker();
+      });
+    }
+    gameStart();
+
+    //Radio Click functions
+    function gamePlay(){
+        clearRadios();
+        //Question01
+        $("#group1 input").on("click", function(){
+          $(this).addClass("checked");
+          if ($("input#answer3").hasClass("checked")){
+            rightAnswers++;
+            unAnswered--;
+            $("#group1 .correctAnswer").show();
+            $("#group1 .incorrectAnswer").hide();
+            //$("#group1").delay(5000).hide(0);
+            //$("#group2").delay(5000).show(0);
+
+            console.log("Q1 correct answer is working")
+          } else if (!$("input#answer3").hasClass("checked")){
+            unAnswered++;
+
+            $("#group1 .incorrectAnswer").show();
+            console.log("Q1 INCORRECT answer is working")
+          } else {
+            wrongAnswers++;
+            $("#group1 .incorrectAnswer").show();
+            $("#group1 .correctAnswer").hide();
+          }
+        });
+
+        //Question02
+        $("#group2 input").on("click", function(){
+          $(this).addClass("checked");
+          if ($("input#answer7").hasClass("checked")){
+            rightAnswers++;
+            unAnswered--;
+            $("#group2 .correctAnswer").show();
+            $("#group2 .incorrectAnswer").hide();
+          } else if (!$("input#answer3").hasClass("checked")){
+            unAnswered++;
+
+            $("#group2 .incorrectAnswer").show();
+          } else {
+            wrongAnswers++;
+            $("#group2 .incorrectAnswer").show();
+            $("#group2 .correctAnswer").hide();
+          }
+        });
+
+        //Question03
+        $("#group3 input").on("click", function(){
+          $(this).addClass("checked");
+          if ($("input#answer12").hasClass("checked")){
+            rightAnswers++;
+            unAnswered--;
+            $("#group3 .correctAnswer").show();
+            $("#group3 .incorrectAnswer").hide();
+          } else if (!$("input#answer3").hasClass("checked")){
+            unAnswered++;
+
+            $("#group3 .incorrectAnswer").show();
+          } else {
+            wrongAnswers++;
+            $("#group3 .incorrectAnswer").show();
+            $("#group3 .correctAnswer").hide();
+          }
+        });
+
+        //Question04
+        $("#group4 input").on("click", function(){
+          $(this).addClass("checked");
+          if ($("input#answer13").hasClass("checked")){
+            rightAnswers++;
+            unAnswered--;
+            $("#group4 .correctAnswer").show();
+            $("#group4 .incorrectAnswer").hide();
+          } else if (!$("input#answer3").hasClass("checked")){
+            unAnswered++;
+            $("#group4 .incorrectAnswer").show();
+          } else {
+            wrongAnswers++;
+            $("#group4 .incorrectAnswer").show();
+            $("#group4 .correctAnswer").hide();
+          }
+        });
+
+        //Question05
+        $("#group5 input").on("click", function(){
+          $(this).addClass("checked");
+          if ($("input#answer19").hasClass("checked")){
+            rightAnswers++;
+            unAnswered--;
+            $("#group5 .correctAnswer").show();
+            $("#group5 .incorrectAnswer").hide();
+            stop();
             $("#startScreen").hide();
-            $("#gameView").show();
-            $("#results").hide();
-            console.log("Button Click is working");
-           
-            
-            setInterval( function() {
-              $("#ticker").html(gameTime--);
-            }, 1000);
-            
+            $("#gameView").delay(5000).hide(0);
+            $("#results").delay(5000).show(0);
+          } else if (!$("input#answer3").hasClass("checked")){
+            unAnswered++;
+            $("#group5 .incorrectAnswer").show();
+            stop();
+            $("#startScreen").hide();
+            $("#gameView").delay(5000).hide(0);
+            $("#results").delay(5000).show(0);
+          } else {
+            wrongAnswers++;
+            $("#group5 .incorrectAnswer").show();
+            $("#group5 .correctAnswer").hide();
+          }
         });
     }
-//    function gameEnd(){
-//        if (gameTime === 0){
-//            $("#startScreen").hide();
-//            $("#gameView").hide();
-//            $("#results").show();
-//            }
-//    }
-    gameStart();
-    radioBuilder();
-    //gameEnd();
+
+    //Score Keeping for Final Screen
+    function scoreBucket(){
+      $("#correctAnswers").text(rightAnswers);
+      $("#incorrectAnswers").text(wrongAnswers);
+      $("#unanswered").text(unAnswered);
+      console.log("score bucket is working");
+    }
+    //Play Game Again
+    function reset(){
+      $("#tryAgain").on("click", function(){
+        $("#correct").text("0");
+        $("#incorrect").text("0");
+        $("#unanswered").text("6");
+        $("#startScreen").show();
+        $("#gameView").hide();
+        $("#results").hide();
+        $(".correctAnswer").hide();
+        $(".incorrectAnswer").hide();
+        $("#ticker").text(gameTime = 60).css('');
+        console.log("reset button is working");
+      });
+
+    }
+    gamePlay();
+    scoreBucket();
+    reset();
 });
-//Question Variables for End of Game
-
-
-//Right and wrong answer Totals
---
+++ assets/js/app_v2.js
@@ -0,0 +1,69 @@
+// JavaScript Document
+
+//game start
+function hideGame(){
+    $("#startScreen").show();
+    //$("#gameView").hide();
+    //$("#results").hide();
+    console.log("hide game is working");
+}
+hideGame();
+function gameStart(){
+    $("#startScreen a").on("click", function(){
+        $("#startScreen").hide();
+        $("#gameView").show();
+        $("#results").hide();
+        console.log("Button Click is working");
+      });
+}
+var gameArray = [{
+  question: "What year did Chris Farley first appear on SNL?",
+  answers: ["1988", "1989", "1990", "1991"],
+  a: "1990",
+},{
+  question: "In Mike Myer's 'Sprockets' Skit, Deter always ended the show with:",
+  answers: ["petting the monkey", "singing", "dancing", "fading to black"],
+  a: "dancing",
+},{
+  question: "In Chevy Chase's Weekend News Bit, he'd proclaim 'I'm Chevy Chase and ____?'",
+  answers: ["this is the news", "for tonight’s top story", "good evening", "you’re not"],
+  a: "you're not",
+},{
+  question: "On 'Wayne's World', Wayne's sidekick is named _____?",
+  answers: ["Garth", "Olaf", "Bart", "Gus"],
+  a: "Garth",
+},{
+  question: "Chris Farley's Matt Foley character always threatened that 'You'll be living in a ____?'",
+  answers: ["shack in the timbers", "shelter in the woods", "van down the river", "truck by the swamp"],
+  a: "van down by the river",
+},{
+  question: "Mike Myer’s character “Simon” like do do what?",
+  answers: ["drawings", "sculptures", "drum solo", "guitar solo", "paintings"],
+  a: "drawings",
+}];
+var number = 0;
+var tabIndexSet = 1;
+//builds the answer radio buttons
+var radioQuestion = gameArray[number].question;
+//builds the answer radio buttons
+var questionListing = gameArray[number].answers;
+
+
+function radioBuilder() {
+  //console.log(gameArray[0].question);
+  //console.log(gameArray[0].answers);
+  //builds the question and radios in this div
+  var result = $("#question01", "#question02");
+  //sets tab index for radio buttons
+  //builds the for loop inside html
+  result.html('');
+  //for loop
+  result.append('<h3>' + gameArray[number].question + '</h3>');
+  for (var i = 0; i < questionListing.length; i++) {
+    result.append('<label><input type="radio" name="usernames" value="' + questionListing[i] + '"tabindex="' + tabIndexSet++ + '" />' + questionListing[i] + '</label>');
+    console.log("question builder is working");
+    }
+    result.append('<div class="correctAnswer"></div>');
+    result.append('<div class="correctAnswer"></div>');
+}
+radioBuilder();
